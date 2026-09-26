a383c0f48171e9686de8ea567023695ab708a310
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-09-26 17:07:41 -0400

    Add getMovie function to fetch movie data
    Created the getMovie() function.
    Implemented an API request to fetch movie data from the OMDb API. Replaced the previous API key as it didn't work.

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const searchForm = document.querySelector(".search-form")
-const ombdApiBaseUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=c971e1aa"
+const ombdApiBaseUrl = "http://www.omdbapi.com/?i=tt3896198&apikey=d56ba9f8"
 
 
 searchForm.addEventListener("submit", function(e) {
     e.preventDefault()
 
     const formData = new FormData(e.target)
-    const queryStr = formData.get("query")
+    const movieSearchQuery = formData.get("query")
+    
+    getMovie(movieSearchQuery)
 })
+
+
+function getMovie(movieSearchQuery) {
+    const queryStr = `${ombdApiBaseUrl}&t=${movieSearchQuery}`
+
+    fetch(queryStr)
+        .then(response => response.json())
+        .then(data => console.log(data))
+}
